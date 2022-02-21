2d4e9bd348615771e1495e8709e0f0f73a8cd24c
wpy1998 <1251499395@qq.com>
2022-02-20 17:00:52 -0800

    commit by wpy in 2022.2.21

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,14 @@
 import Hardware.HardwareMessage;
 import HttpOperation.StreamFunction;
-import net.i2cat.netconf.NetconfSession;
-import net.i2cat.netconf.SessionContext;
-import net.i2cat.netconf.errors.NetconfProtocolException;
-import net.i2cat.netconf.errors.TransportException;
-import net.i2cat.netconf.errors.TransportNotRegisteredException;
 
 import java.io.IOException;
-import java.net.URI;
 
 public class App {
-    public static void main(String[] args) throws IOException, TransportNotRegisteredException, NetconfProtocolException, TransportException {
+    public static void main(String[] args) throws IOException{
         HardwareMessage hdMessage = new HardwareMessage();
         hdMessage.refresh();
         System.out.println(hdMessage.device_ip + ", " + hdMessage.device_mac);
-        String url = "http://192.168.1.3:8181/restconf/operations/talker:";
+        String url = "http://10.2.25.85:8181/restconf/operations/talker:";
         StreamFunction streamFunction = new StreamFunction();
         streamFunction.join(url, "hello join");
         streamFunction.test(url);
